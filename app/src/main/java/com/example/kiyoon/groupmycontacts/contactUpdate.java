ea2ce30feb59de10810b6292c57eae09a4766edf
qaispak <qaisbadsha@gmail.com>
2015-09-12 14:43:13 -0400

    Added different pages/skeleton
    3 different pages for three different cases

--- app/src/main/java/com/example/kiyoon/groupmycontacts/contactUpdate.java
+++ app/src/main/java/com/example/kiyoon/groupmycontacts/contactUpdate.java
@@ -1,9 +1,11 @@
 package com.example.kiyoon.groupmycontacts;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by badsha on 9/12/2015.
  */
-public class contactUpdate {
+public class contactUpdate extends AppCompatActivity {
 
 
 
---
+++ app/src/main/java/com/example/kiyoon/groupmycontacts/secondpage.java
@@ -0,0 +1,32 @@
+package com.example.kiyoon.groupmycontacts;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by badsha on 9/12/2015.
+ */
+public class secondpage extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_page1);
+
+        Button textButton = (Button) findViewById(R.id.textButtonID1);
+        textButton.setText("Text Contacts!");
+
+        textButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                // call the method to SMS user
+                finish();
+            }
+
+        });
+    }
+}
