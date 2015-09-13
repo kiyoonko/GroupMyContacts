95b5534b97c03aafdc995f45999dfb283ce21d67
mzwee <mzwee95@gmail.com>
2015-09-13 07:08:34 -0400

    merged listview and morgan's code

--- app/src/main/java/com/example/kiyoon/groupmycontacts/secondpage2.java
+++ app/src/main/java/com/example/kiyoon/groupmycontacts/secondpage2.java
@@ -2,31 +2,114 @@ package com.example.kiyoon.groupmycontacts;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import com.firebase.client.AuthData;
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by badsha on 9/12/2015.
  */
 public class secondpage2 extends AppCompatActivity {
 
+    private Button btnSendSMS;
+    private Firebase ref;
+    private Firebase usrRef;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_page2);
 
-        Button textButton = (Button) findViewById(R.id.textButtonID2);
-        textButton.setText("Text Contacts!");
+        //Set up firebase
+        Firebase.setAndroidContext(this);
+        ref = new Firebase("https://GroupMyContacts.firebaseio.com");
+        final List<String> contacts = new ArrayList<String>();
 
-        textButton.setOnClickListener(new View.OnClickListener() {
+        AuthData authData = ref.getAuth();
+        if (authData != null) {
+            usrRef = new Firebase("https://groupmycontacts.firebaseio.com/users/" + authData.getUid() + "/contacts");
+            usrRef.addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                    Contact user = dataSnapshot.getValue(Contact.class);
+                    if (user.getCategory() == 2) {
+                        contacts.add(user.getName() + " " + user.getPhoneNum());
+                    }
+                }
 
-            @Override
-            public void onClick(View view) {
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
 
-                // call the method to SMS user
-                finish();
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onCancelled(FirebaseError firebaseError) {
+
+                }
+            });
+
+        }
+
+        final ListView listView = (ListView) findViewById(R.id.list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, contacts);
+        listView.setAdapter(adapter);
+
+        btnSendSMS = (Button) this.findViewById(R.id.btn_SendSMS);
+        btnSendSMS.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("SMS", "Clicked the button!!");
+                String numbers[] = {"+19195970133", "+12407784399", "+17345459884"
+                };
+                for (String number : numbers) {
+                    com.example.kiyoon.groupmycontacts.Util.sendSMS(number, "Your friend needs your help. Can you come? Respond with Yes or No. Yes will also provide his/her location", secondpage2.this);
+                }
             }
-
         });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
